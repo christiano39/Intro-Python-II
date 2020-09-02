cc5c0fb0f1e72bea8a0779ca88a017871b897dd8
christiano39 <carneson39@gmail.com>
2020-09-01 21:31:24 -0600

    create player and room classes, set up REPL for gameplay

--- src/adv.py
+++ src/adv.py
@@ -1,22 +1,23 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+	'outside':  Room("Outside Cave Entrance",
+					 "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+	'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+	'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+	'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+	'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
@@ -38,6 +39,7 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player = Player("Christian", room['outside'])
 
 # Write a loop that:
 #
@@ -49,3 +51,39 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+print(f'\nWelcome, {player.name}')
+
+while True:
+	print()
+	print(player.current_room.name)
+	print(player.current_room.description)
+	choice = input('Which direction would you like to go? ')
+
+	if choice == 'q':
+		print('Exiting game...')
+		break
+	elif choice == 'n':
+		if player.current_room.n_to:
+			player.current_room = player.current_room.n_to
+		else:
+			print()
+			print("There is nothing for you that way, try a new direction...")
+	elif choice == 's':
+		if player.current_room.s_to:
+			player.current_room = player.current_room.s_to
+		else:
+			print()
+			print("There is nothing for you that way, try a new direction...")
+	elif choice == 'e':
+		if player.current_room.e_to:
+			player.current_room = player.current_room.e_to
+		else:
+			print()
+			print("There is nothing for you that way, try a new direction...")
+	elif choice == 'w':
+		if player.current_room.w_to:
+			player.current_room = player.current_room.w_to
+		else:
+			print()
+			print("There is nothing for you that way, try a new direction...")
